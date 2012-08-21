52ddf8a19d5a2b8f8a398cb63bc720e16ee39841
Yi Wang <wangyi@farefore.com>
2012-08-21 15:23:18 +0800

    Clear table_content element inner text if POST to matrx-server succeeds.

--- matrix-client.js
+++ matrix-client.js
@@ -16,9 +16,9 @@ function mcDrawMatrix(table_content_id, matrix_server_path, table_id) {
         if (render.readyState == 4 && render.status == 200) {
             document.getElementById(table_id).innerHTML =
                 render.responseText;
+            document.getElementById(table_content_id).innerHTML = "";
         }
     }
     render.open("POST", matrix_server_path, true);
     render.send(document.getElementById(table_content_id).innerHTML);
-    document.getElementById(table_content_id).innerHTML = "";
 }
